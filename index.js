ae8550f9938c85436b8c58224e6d6a1427205da2
wangd-aa <wangd-aa@glodon.com>
2023-09-19 09:56:29 +0800

    update

--- index.js
+++ index.js
@@ -13,38 +13,38 @@ const dirPath = path.resolve(__dirname, "pictures");
 
 // 获取当前日期并格式化为需要的文件名格式
 const currentDate = new Date().toLocaleDateString("en", {
-  year: "numeric",
-  month: "2-digit",
-  day: "2-digit",
+	year: "numeric",
+	month: "2-digit",
+	day: "2-digit",
 });
 
 async function downloadImage(url, destination) {
-  return new Promise((resolve, reject) => {
-    const writeStream = fs.createWriteStream(destination);
-    request(url)
-      .pipe(writeStream)
-      .on("close", () => {
-        resolve();
-      })
-      .on("error", (err) => {
-        reject(err);
-      });
-  });
+	return new Promise((resolve, reject) => {
+		const writeStream = fs.createWriteStream(destination);
+		request(url)
+			.pipe(writeStream)
+			.on("close", () => {
+				resolve();
+			})
+			.on("error", (err) => {
+				reject(err);
+			});
+	});
 }
 
 async function main() {
-  try {
-    if (!fs.existsSync(dirPath)) {
-      await fs.mkdir(dirPath);
-    }
+	try {
+		// if (!fs.existsSync(dirPath)) {
+		//   await fs.mkdir(dirPath);
+		// }
 
-    const filePath = path.join(dirPath, `${currentDate}.png`);
-    await downloadImage(url, filePath);
+		const filePath = path.join(dirPath, `${currentDate}.png`);
+		await downloadImage(url, filePath);
 
-    console.log(`Image downloaded and saved to: ${filePath}`);
-  } catch (error) {
-    console.error("An error occurred:", error);
-  }
+		console.log(`Image downloaded and saved to: ${filePath}`);
+	} catch (error) {
+		console.error("An error occurred:", error);
+	}
 }
 
 main();
